c54258a08113f40308fbb665d22d5feb449d6e08
Alejandro Fallad <alexfamas@gmail.com>
2020-01-21 21:23:01 -0600

    Initial EF migration

---
+++ src/Services/Catalog/Migrations/20200122032252_InitialMigration.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Catalog.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "EntityFrameworkHiLoSequence",
+                incrementBy: 10);
+
+            migrationBuilder.CreateTable(
+                name: "CatalogBrands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Brand = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogBrands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Type = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    AvailableStock = table.Column<int>(nullable: false),
+                    BrandId = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    LongName = table.Column<string>(nullable: true),
+                    MaxStockThreshold = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    ShortName = table.Column<string>(nullable: true),
+                    Sku = table.Column<string>(nullable: true),
+                    Stock = table.Column<int>(nullable: false),
+                    TypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CatalogItems_CatalogBrands_BrandId",
+                        column: x => x.BrandId,
+                        principalTable: "CatalogBrands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CatalogItems_CatalogTypes_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "CatalogTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatalogItems_BrandId",
+                table: "CatalogItems",
+                column: "BrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatalogItems_TypeId",
+                table: "CatalogItems",
+                column: "TypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CatalogItems");
+
+            migrationBuilder.DropTable(
+                name: "CatalogBrands");
+
+            migrationBuilder.DropTable(
+                name: "CatalogTypes");
+
+            migrationBuilder.DropSequence(
+                name: "EntityFrameworkHiLoSequence");
+        }
+    }
+}
