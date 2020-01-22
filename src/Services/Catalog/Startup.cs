af7c1db80942cc1dee05f55ba0fa2d4a03224e76
Alejandro Fallad <alexfamas@gmail.com>
2020-01-22 08:36:38 -0600

    Swagger support

--- src/Services/Catalog/Startup.cs
+++ src/Services/Catalog/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Catalog
 {
@@ -50,6 +51,11 @@ namespace Catalog
                 options.ConfigureWarnings(warnings => warnings.Throw(
                     RelationalEventId.QueryClientEvaluationWarning));
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -69,6 +75,12 @@ namespace Catalog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
