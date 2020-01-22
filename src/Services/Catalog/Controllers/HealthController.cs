c644996c4bf04287fd6710beb242b77c5ea19fe2
Alejandro Fallad <alexfamas@gmail.com>
2020-01-22 08:36:51 -0600

    Is alive endpoint

---
+++ src/Services/Catalog/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("isAlive")]
+        public IActionResult IsAlive()
+        {
+            return this.NoContent();
+        }
+
+
+    }
+}
